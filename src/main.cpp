a13030cb9ea11db2d906237520743ec30e8c7d92
Aadeshveer <24b0926@iitb.ac.in>
2025-09-06 23:55:51 +0800

    Added color.hpp and rewrote experimental main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,18 @@
-#include <iostream>
-#include <cmath>
+#include "vec3.hpp"
+#include "color.hpp"
 
 int main() {
 
-    int image_width = 1024;
+    int image_width = 512;
     int image_height = 512;
 
     std::cout << "P3\n" << image_width << ' ' << image_height << '\n' << 255 << std::endl;
     for(int i=0 ; i<image_height; i++) {
         std::clog << "\rLines remaining: " << (image_height-i) << ' ' << std::flush;
         for(int j=0; j<image_width; j++) {
-            auto r=1.0*j/(image_width-1);
-            auto b=1.0*i/(image_height-1);
-            auto g=0.0;
-
-            int ir = int(255.999 * r);
-            int ig = int(255.999 * g);
-            int ib = int(255.999 * b);
-            
-            std::cout << ir << ' ' << ig << ' ' << ib << std::endl;
+            color pixel_clr = color(1.0*i/(image_height-1), 1.0*j/(image_width-1), 0.0);
+            write_color(std::cout, pixel_clr);
         }
     }
-    std::clog << "   Done   \n" << std::endl;
+    std::clog << "\r         Done         \n" << std::endl;
 }
